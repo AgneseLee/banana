cd7b0e6e83d1cc011dc273fb424ad40a15b66934
agnsesli <agneselee@163.com>
2018-10-17 00:42:48 +0800

    remove node_modules

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+ 
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+}
+
