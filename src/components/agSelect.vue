3ae499fec0f6c3ebfa9e585b07171c773f66780f
agnsesli <agneselee@163.com>
2018-11-06 00:22:54 +0800

    select组件样式

--- src/components/agSelect.vue
+++ src/components/agSelect.vue
@@ -1,19 +1,16 @@
 <template>
-    <div class="select">
-        <div class="inner" v-clickOut="test">
-            <div class="inputWrapper" @click="showOptions = !showOptions">
-                <input type="text" readonly placeholder="请选择菜品" :value="selected">
-                <span class="iconfont icon-zhankaishangxia"></span>
-            </div>
-            <ul class="options" v-show="showOptions">
-                <li
-                    v-for="(item, index) in options"
-                    :key="index"
-                    @click="choose(item.value)"
-                >{{item.value}}</li>
-            </ul>
-        </div>
+  <div class="select">
+    <div class="inner" v-clickOut="hideList">
+      <div class="inputWrapper">
+        <input type="text" readonly placeholder="请选择菜品" :value="selected">
+        <span class="iconfont"></span>
+      </div>
+      <ul class="options" v-show="showOptions">
+        <li v-for="(item, index) in options" :key="index" @click="choose(item.name)">{{item.name}}</li>
+      </ul>
     </div>
+  </div>
+
 </template>
 
 
@@ -24,8 +21,9 @@ export default {
     choose(name) {
       console.log(name);
     },
-    test() {
+    hideList() {
       console.log("outside");
+      this.showOptions = !this.showOptions;
     }
   },
   data() {
@@ -53,3 +51,55 @@ export default {
   }
 };
 </script>
+
+<style>
+body {
+  background: #ffbd1e;
+}
+ul,
+li {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+/*下拉框样式*/
+#select {
+  margin: 100px;
+  background: rgba(0, 0, 0, 0);
+  width: 249px;
+  height: 40px;
+  font-family: "微软雅黑";
+  font-size: 18px;
+  color: white;
+  border: 1px #1a1a1a solid;
+  border-radius: 5px;
+}
+.select-head {
+  overflow: hidden;
+  width: 249px;
+  height: 40px;
+  box-sizing: border-box;
+  padding: 0 10px;
+  line-height: 40px;
+}
+.select-head .select-head-cont {
+  float: left;
+}
+.select-head .select-icon {
+  float: right;
+}
+.option {
+  text-indent: 10px;
+  margin-top: 1px;
+  width: 249px;
+  color: black;
+  background: rgba(236, 111, 111, 0.1);
+  line-height: 25px;
+  border: 1px #cfcfcf solid;
+  display: none;
+}
+.option-item:hover {
+  background: rgba(204, 106, 67, 0.3);
+}
+</style>
+
